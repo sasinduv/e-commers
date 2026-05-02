4e2348186bb51583c17a008ec75d5f308868e821
sasinduv <sasindugihan123456@gmail.com>
2026-05-02 20:07:18 +0530

    feat: enhance styling and structure of App.css, create Header strip and Header component

--- src/components/CountryDropdown/index.js
+++ src/components/CountryDropdown/index.js
@@ -1,16 +1,21 @@
-import Button from '@mui/material/Button';
+import Button from "@mui/material/Button";
 import { FaAngleDown } from "react-icons/fa6";
 
-const CountryDropdown= () => {
-    return(
-        <div className="countryDropdown">
-            <Button className="countryDrop">
-                <span >Your Location</span>
-                <span >Sri Lanka</span>
+const CountryDropdown = () => {
+    return (
+        <Button className="countryDrop">
+
+            <div className="info">
+                <span className="label">Your Location</span>
+                <span className="name">Sri Lanka</span>
+            </div>
+
+            <span className="arrow">
                 <FaAngleDown />
-            </Button>
-        </div>
-    )
-}
+            </span>
+
+        </Button>
+    );
+};
 
 export default CountryDropdown;
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,36 +1,88 @@
 import { Link } from "react-router-dom";
 
-import { FaAngleDown } from "react-icons/fa6";
 import CountryDropdown from "../CountryDropdown";
+import Button from "@mui/material/Button";
+
+import { IoSearchCircleOutline } from "react-icons/io5";
+import { FaRegCircleUser } from "react-icons/fa6";
+import { BsCart4 } from "react-icons/bs";
 
 const Header = () => {
-    return(
+    return (
         <>
-        <div className="headerWrapper" >
-            <div className="top-strip bg-purple">
-                <div className="container">
-                    <p className="mb-0 mt-0 text-center">Welcome to our <b>online</b> store!</p>
-                </div>
-            </div>
+            <div className="headerWrapper">
 
-            <div className="header">
-                <div className="container">
-                    <div className="row">
-                        <div className="logoWrapper d-flex align-items-center col-sm-2">
-                            <Link to={"/"}>
-                                 <img src="/images/logo.png" alt="Logo" />
-                            </Link>
-                        </div>
-
-                        <div className="col-sm-10 d-flex align-items-center justify-content-end part2">
-                            <CountryDropdown />
-                        </div>
+                {/* Top Strip */}
+                <div className="top-strip bg-purple">
+                    <div className="container">
+                        <p className="mb-0 mt-0 text-center">
+                            Welcome to our <b>online</b> store!
+                        </p>
                     </div>
                 </div>
+
+                {/* Main Header */}
+                <header className="header">
+                    <div className="container">
+                        <div className="headerRow">
+
+                            {/* Logo */}
+                            <div className="logoWrapper">
+                                <Link to={"/"}>
+                                    <img src="/images/logo.png" alt="Logo" />
+                                </Link>
+                            </div>
+
+                            {/* Country Dropdown */}
+                            <div className="part2">
+                                <CountryDropdown />
+                            </div>
+
+                            {/* Search Bar */}
+                            <div className="headerSearch">
+                                <input
+                                    type="text"
+                                    placeholder="Search for products..."
+                                />
+
+                                <Button>
+                                    <IoSearchCircleOutline />
+                                </Button>
+                            </div>
+
+                            {/* Right Side */}
+                            <div className="part3">
+
+                                <Button className="circle">
+                                    <FaRegCircleUser />
+                                </Button>
+
+                                <div className="cartTab">
+
+                                    <span className="price">$3.50</span>
+
+                                    <div className="position-relative">
+
+                                        <Button className="circle">
+                                            <BsCart4 />
+                                        </Button>
+
+                                        <span className="cartCount">
+                                            2
+                                        </span>
+
+                                    </div>
+                                </div>
+
+                            </div>
+
+                        </div>
+                    </div>
+                </header>
+
             </div>
-        </div>
         </>
-    )
-}
+    );
+};
 
 export default Header;
