4551fa5212ffee375737c31b6b245da3642eeb5a
sasinduv <sasindugihan123456@gmail.com>
2026-05-03 10:29:17 +0530

    feat: enhance navigation component with additional links and styling

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,5 +1,18 @@
 import Button from "@mui/material/Button"
 import { TiThMenuOutline } from "react-icons/ti";
+import { RiHome3Line } from "react-icons/ri";
+import { AiOutlineShop } from "react-icons/ai";
+import { GiCircuitry } from "react-icons/gi";
+import { GiHotDog } from "react-icons/gi";
+import { RiShoppingBag3Line } from "react-icons/ri";
+import { FaMobileScreenButton } from "react-icons/fa6";
+import { PiMicrophoneStageLight } from "react-icons/pi";
+
+
+
+
+
+import { Link } from "react-router-dom";
 
 
 
@@ -15,8 +28,35 @@ const Navigation = () => {
                         </Button>
                     </div>
 
-                    <div className="col-sm-9 navPart2">
-
+                    <div className="col-sm-9 navPart2 d-flex align-items-center">
+                        <ul className="list list-inline ml-alto">
+                            <li className="list-inline-item">
+                                <Link to="/"><RiHome3Line /> &nbsp; Home</Link>
+                            </li>
+                            <li className="list-inline-item">
+                                <Link to="/"><AiOutlineShop /> &nbsp;  Shop</Link>
+                            </li>
+                            <li className="list-inline-item">
+                                <Link to="/"><GiCircuitry />
+&nbsp; Electronics</Link>
+                            </li>
+                            <li className="list-inline-item">
+                                <Link to="/"><GiHotDog />
+ &nbsp;Bakery</Link>
+                            </li>
+                            <li className="list-inline-item">
+                                <Link to="/"> <RiShoppingBag3Line />
+ &nbsp;Grocery</Link>
+                            </li>
+                            <li className="list-inline-item">
+                                <Link to="/"><PiMicrophoneStageLight />
+&nbsp; Blog</Link>
+                            </li>
+                            <li className="list-inline-item">
+                                <Link to="/"><FaMobileScreenButton />
+&nbsp; Contact Us</Link>
+                            </li>
+                        </ul>
                     </div>
 
                 </div>
